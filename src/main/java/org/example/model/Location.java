572db515ed247b71e139170abbfe3e95bbfdc64f
Carlos <sscarlos3435@gmail.com>
2023-11-01 12:50:17 +0000

    First Commit

---
+++ src/main/java/org/example/model/Location.java
@@ -0,0 +1,7 @@
+package org.example.model;
+
+public class Location {
+    private double latitude;
+    private double longitude;
+    private String island;
+}
---
+++ src/main/java/org/example/model/Weather.java
@@ -0,0 +1,13 @@
+package org.example.model;
+
+import java.time.Instant;
+
+public class Weather {
+    private float temp;
+    private int pressure;
+    private int humidity;
+    private float wind;
+    private Instant ts;
+    private Location location;
+
+}
